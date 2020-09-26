65b7922989c3efda9b387f28ac170a450f53b449
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2020-09-26 17:58:52 +0900

    set nextjs

--- client/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const App = () => {
-  return <div>gg</div>;
-};
-
-export default App;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -10,17 +10,6 @@ const indexPage = () => {
         <title>메인페이지</title>
       </Head>
       <Layout>
-        <div>
-          g111g
-          <div>
-            <Link href="/playground">
-              <a>go playground</a>
-            </Link>
-          </div>
-          <div>
-            <a href="/playground">go playground without link</a>
-          </div>
-        </div>
       </Layout>
     </>
   );
