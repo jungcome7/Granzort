93d473c8cb14f38ea979306d601277a3d0b9344f
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2020-09-27 16:41:13 +0900

    create sample counter

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { increment, decrement } from '../actions';
 
-const App = () => {
-  const counter = useSelector((state: any) => state.count);
-  const isLogged = useSelector((state: any) => state.logged);
-  const dispatch = useDispatch();
+// const App = () => {
+//   const counter = useSelector((state: any) => state.count);
+//   const isLogged = useSelector((state: any) => state.logged);
+//   const dispatch = useDispatch();
 
-  return (
-    <div>
-      <h1>Counter {counter}</h1>
-      <button onClick={() => dispatch(increment())}>++++</button>
-      <button onClick={() => dispatch(decrement())}>----</button>
-      <h1>isLogged {isLogged + ''}</h1>
-    </div>
-  );
+//   return (
+//     <div>
+//       <h1>Counter {counter}</h1>
+//       <button onClick={() => dispatch(increment())}>++++</button>
+//       <button onClick={() => dispatch(decrement())}>----</button>
+//       <h1>isLogged {isLogged + ''}</h1>
+//     </div>
+//   );
+// };
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [something, setSomething] = useState(false);
+  const clickHandler = () => {
+    setCount(count + 1);
+  };
+  useEffect(() => {
+    setCount(count + 1);
+  }, []);
+  return <div onClick={clickHandler}>이것은 카운트 = {count}</div>;
 };
 
 export default App;
